e9f963c2da947d85872cf38a0323712bc5c8bee1
soyunhan <sooho15@nate.com>
2023-04-06 21:24:43 +0900

    sort 리팩토링

--- Week_1/02.js
+++ Week_1/02.js
@@ -1,8 +1,6 @@
-function solutions(strings, n) {
-    // 배열의 sort() 메소드는 인자로 비교함수를 받습니다.
-    // 이 비교함수는 배열의 두 요소를 비교하여 정렬 순서를 결정합니다. 
-    // a[n]이 더 작은 경우 -1, a[n]이 더 큰 경우 1, 같은 경우 0을 반환하는 조건 
-    return strings.sort((a, b) => a[n] < b[n] ? -1 : a[n] > b[n] ? 1 : 0);
-}
+// 주어진 문자열 배열(strings)을 n번째 문자열을 기준으로 오름차순 정렬하는 함수
+const solutions = (strings, n) =>
+  strings.sort((a, b) => a[n].localeCompare(b[n]));
 
-console.log(solutions(["sun", "bed", "car"], 1));// ["car", "bed", "sun"]
+// ["sun", "bed", "car"] 배열을 n = 1로 정렬하여 결과 출력
+console.log(solutions(["sun", "bed", "car"], 1)); // ["car", "bed", "sun"] // ["sun", "bed", "car"] 배열을 n = 1로 정렬하여 결과 출력하는 코드
