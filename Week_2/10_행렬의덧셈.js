586289987eff22a0acbddd0c954ab0285e31ec60
soyunhan <sooho15@nate.com>
2023-04-07 17:39:27 +0900

    x만큼간격

---
+++ Week_2/10_행렬의덧셈.js
@@ -0,0 +1,16 @@
+function solution(arr1, arr2) {
+    var answer = [[]];
+    console.log(arr1[0][0], arr1[0][1])
+    console.log(arr1[1][0], arr1[1][1])
+    console.log(arr2[0][0], arr2[0][1])
+    console.log(arr2[1][0], arr2[1][1])
+
+
+    console.log('-------------')
+    console.log(arr1[0][0], arr1[0][1])
+
+    return answer;
+}
+
+
+console.log(solution([[1,2],[2,3]] , [[3,4],[5,6]]))
