297ce35e5e3dc17491bf2f9e6ff8b45249c7664d
soyunhan <sooho15@nate.com>
2023-04-07 14:45:56 +0900

    알고

--- Week_2/04_두정수사이의합.js
+++ Week_2/04_두정수사이의합.js
@@ -4,27 +4,23 @@ let fs = require('fs');
 let n = fs.readFileSync('input.txt').toString().split(' ');
 
 
-let a = Number(n[0])
-let b = Number(n[1])
-console.log(a)
+let a = Number(n[0]) // n 배열의 첫번째 값을 a에 숫자로 변환하여 할당
+let b = Number(n[1]) // n 배열의 두번째 값을 b에 숫자로 변환하여 할당
+console.log(a) // a 값 출력
 
 
-function solution(a, b) {
-    var answer = 0;
-    if(a < b){
-        for(let i = a ; i <=b ; i++){
-            answer += i
+function solution(a, b) { // a, b 매개변수를 받는 solution 함수 선언
+    var answer = 0; // answer 변수를 0으로 초기화
+    if(a < b){ // a가 b보다 작은 경우
+        for(let i = a ; i <=b ; i++){ // a부터 b까지 반복문 실행
+            answer += i // answer에 i를 더함
         }
-    }else if(a > b){
-        for(let i = b ; i <= a; i++){
-            answer += i
+    }else if(a > b){ // a가 b보다 큰 경우
+        for(let i = b ; i <= a; i++){ // b부터 a까지 반복문 실행
+            answer += i // answer에 i를 더함
         }
     }
-    return answer;
+    return answer; // answer 반환
 }
 
-console.log(solution(a ,b))
-
-//두 수를 따로따로 가져온다
-//수의 갯수로 돌려준다
-//처음 수가 크면 두수를 바꿔준다
+console.log(solution(a ,b)) // solution 함수에 a, b를 인자로 전달하여 결과 출력
