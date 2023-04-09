14ccc149082773420c18a0bb99e153e1792b6739
soyunhan <sooho15@nate.com>
2023-04-09 17:52:08 +0900

    filer예제

--- Week_2/02_짝수와홀수.js
+++ Week_2/02_짝수와홀수.js
@@ -12,11 +12,16 @@
 // console.log(solution(2)); // solution 함수를 호출하고, 인자로 2를 전달하여 출력한다.
 
 
-function solution(num) {
-    var answer = ''; 
-    //삼항연산자 리팩토링
-    return (num % 2 === 0 ? answer = 'Even' : answer = 'Odd'); // answer를 반환한다.
-}
+// function solution(num) {
+//     var answer = ''; // 빈 문자열 변수 answer를 선언한다.
+//     //삼항연산자 리팩토링
+//     return (num % 2 === 0 ? answer = 'Even' : answer = 'Odd'); // answer를 반환한다.
+// }
 
-console.log(solution(2)); // solution 함수를 호출하고, 인자로 2를 전달하여 출력한다.
+// // solution 함수를 호출하고, 인자로 2를 전달하여 출력한다.
+// console.log(solution(2));
 
+//filter예제
+const arr1 = ['박수호', '건담', '레고', '헬로카봇']
+const result = arr1.filter( arr1 => arr1.length > 2);
+console.log(result)
