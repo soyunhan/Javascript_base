f1f0c7d1ab5446e5aad9e56e0edb919e27c00645
soyunhan <sooho15@nate.com>
2023-04-09 18:45:21 +0900

    가운데글자 리팩토링

--- Week_2/03._가운데글자.js
+++ Week_2/03._가운데글자.js
@@ -1,24 +1,13 @@
 // fs 모듈을 불러온다.
 let fs = require('fs');
 // input.txt 파일을 읽어와서 문자열로 변환한 후 공백을 기준으로 나눈다.
-let n = fs.readFileSync('input.txt').toString().split('\n');
+let n = fs.readFileSync('input.txt').toString().split(' ');
 
 
 function solution(s) {
     var answer = '';
-
-    // 문자열의 길이가 짝수인 경우
-    if(s.length % 2 === 0){
-      let mid = Math.floor(s.length / 2) -1;
-      // 가운데 두 글자를 추출하여 answer에 저장
-      answer = s.substring(mid, mid + 2); 
-    }else{ // 문자열의 길이가 홀수인 경우
-        let mid = Math.floor(s.length / 2);
-        // 가운데 한 글자를 추출하여 answer에 저장
-        answer = s.substring(mid, mid + 1);
-    }
-
-    return answer;
+    let mid = Math.floor(s.length / 2)
+    return (s.length % 2 === 0) ? answer = s.substring(mid -1, mid + 1) : answer = s.substring(mid, mid + 1);
 }
 
 console.log(solution(n[0])); // "c"
