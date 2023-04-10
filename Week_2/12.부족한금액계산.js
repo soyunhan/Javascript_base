a467e63da954d908b7eada691f77b0cef5abafac
soyunhan <sooho15@nate.com>
2023-04-10 09:41:46 +0900

    12부족한금액은?

--- Week_2/12.부족한금액계산.js
+++ Week_2/12.부족한금액계산.js
@@ -17,13 +17,11 @@
 function solution(price, money, count) {
     var answer = -1;
     let sum = 0
-    for(let i = 1; i <= count; i++){
-        // sum += Number(price * i)
-        answer += price * i
-        console.log(answer)
+    for(let i =1; i<= count ; i++){
+        sum += price * i
     }
-    sum -= money
-    return answer * sum;
+    answer = money - sum
+    return (answer < 0 ? answer = answer * -1 : 0);
 }
 
 console.log(solution(3, 20, 4))
