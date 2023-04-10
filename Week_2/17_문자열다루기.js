c7d32ab2f9f5b17b76c6a1d7783c703acc1aee84
soyunhan <sooho15@nate.com>
2023-04-10 12:19:40 +0900

    문제

--- Week_2/17_문자열다루기.js
+++ Week_2/17_문자열다루기.js
@@ -1,3 +1,10 @@
+// 문자열 s의 길이가 4 혹은 6이고, 숫자로만 구성돼있는지 확인해주는 함수, solution을 완성하세요.
+// 예를 들어 s가 "a234"이면 False를 리턴하고 "1234"라면 True를 리턴하면 됩니다.
+
+// 제한 사항
+// s는 길이 1 이상, 길이 8 이하인 문자열입니다.
+// s는 영문 알파벳 대소문자 또는 0부터 9까지 숫자로 이루어져 있습니다.
+
 function solution(s) {
 
     var answer = true;
@@ -10,7 +17,7 @@ function solution(s) {
     if (/[^0-9]/.test(s)) {
       return false;
     }
-    
+
     // parseInt() 함수는 문자열을 정수로 변환하는 JavaScript 내장 함수입니다. 
     const num1 = parseInt(s);
     return !isNaN(num1);
